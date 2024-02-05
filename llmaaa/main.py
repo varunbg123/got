f737a6092a5cab316abe1bce6c45ae938d87d31b
el18vg <97689448+varunbg123@users.noreply.github.com>
2024-02-05 19:21:27 +0000

    updated to include llmaa

---
+++ llmaaa/main.py
@@ -0,0 +1,5 @@
+from llama_cpp import Llama
+llm = Llama(model_path="model2.gguf", n_gpu_layers=30, n_ctx=3584, n_batch=521, verbose=True)
+# adjust n_gpu_layers as per your GPU and model
+output = llm("Q: Name the planets in the solar system? A: ", max_tokens=32, stop=["Q:", "\n"], echo=True)
+print(output)
